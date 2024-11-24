56fe5acefe5b09c6c06c9de8ea0796375da9372b
GabSch24 <gabriel.ferri@edu.pucrs.br>
2024-11-24 17:49:30 -0300

    fim

--- fds/target/classes/db/data.sql
+++ fds/target/classes/db/data.sql
@@ -1,8 +1,25 @@
-INSERT INTO clientebd (codigo,email,nome) VALUES (1,'lusa@dell','Lusa');
-INSERT INTO clientebd (codigo,email,nome) VALUES (2,'altemir@yahoo','Altemir');
+INSERT INTO clientebd (codigo, email, nome) VALUES (1, 'luciana@dell.com', 'Luciana');
+INSERT INTO clientebd (codigo, email, nome) VALUES (2, 'altemir@yahoo.com', 'Altemir');
+INSERT INTO clientebd (codigo, email, nome) VALUES (3, 'mariana@gmail.com', 'Mariana');
+INSERT INTO clientebd (codigo, email, nome) VALUES (4, 'carlos@hotmail.com', 'Carlos');
+INSERT INTO clientebd (codigo, email, nome) VALUES (5, 'ana.paula@outlook.com', 'Ana Paula');
+INSERT INTO clientebd (codigo, email, nome) VALUES (6, 'ricardo.ti@empresa.com', 'Ricardo');
+INSERT INTO clientebd (codigo, email, nome) VALUES (7, 'priscila.lima@protonmail.com', 'Priscila Lima');
+INSERT INTO clientebd (codigo, email, nome) VALUES (8, 'joao.silva@ymail.com', 'João Silva');
+INSERT INTO clientebd (codigo, email, nome) VALUES (9, 'claudia_rocha@live.com', 'Cláudia Rocha');
+INSERT INTO clientebd (codigo, email, nome) VALUES (10, 'eduardo_borges@gmail.com', 'Eduardo Borges');
 
-INSERT INTO aplicativobd (codigo,nome,custo_mensal) VALUES (1,'Zap-Zap','1999.00');
-INSERT INTO aplicativobd (codigo,nome,custo_mensal) VALUES (2,'Zop-Zop','1000.00');
 
-INSERT INTO assinaturabd (aplicativo_codigo,cliente_codigo,codigo,fim_vigencia,inicio_vigencia) VALUES (1, 1, 1, '24/11/2025', '24/11/2024');
-INSERT INTO assinaturabd (aplicativo_codigo,cliente_codigo,codigo,fim_vigencia,inicio_vigencia) VALUES (1, 2, 2, '24/11/2025', '24/11/2024');
+
+INSERT INTO aplicativobd (codigo, nome, custo_mensal) VALUES (1, 'Zap-Zap', 1999.00);
+INSERT INTO aplicativobd (codigo, nome, custo_mensal) VALUES (2, 'StreamyPlay', 29.90);
+INSERT INTO aplicativobd (codigo, nome, custo_mensal) VALUES (3, 'Taskify', 49.99);
+INSERT INTO aplicativobd (codigo, nome, custo_mensal) VALUES (4, 'EduLearn', 120.00);
+INSERT INTO aplicativobd (codigo, nome, custo_mensal) VALUES (5, 'HealthTrack', 79.90);
+
+
+INSERT INTO assinaturabd (aplicativo_codigo, cliente_codigo, codigo, fim_vigencia, inicio_vigencia) VALUES (1, 1, 1, '26/11/2024', '26/11/2023');
+INSERT INTO assinaturabd (aplicativo_codigo, cliente_codigo, codigo, fim_vigencia, inicio_vigencia) VALUES (1, 2, 2, '13/01/2024', '13/12/2023');
+INSERT INTO assinaturabd (aplicativo_codigo, cliente_codigo, codigo, fim_vigencia, inicio_vigencia) VALUES (2, 3, 3, '15/07/2024', '01/06/2024');
+INSERT INTO assinaturabd (aplicativo_codigo, cliente_codigo, codigo, fim_vigencia, inicio_vigencia) VALUES (3, 4, 4, '31/10/2024', '31/09/2024');
+INSERT INTO assinaturabd (aplicativo_codigo, cliente_codigo, codigo, fim_vigencia, inicio_vigencia) VALUES (5, 5, 5, '12/09/2024', '28/08/2024');
